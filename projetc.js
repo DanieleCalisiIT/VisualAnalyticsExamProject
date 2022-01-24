66ad1a2b3369435f894b0fb925727eb7e82d8ee1
AquilinaGreta <90396995+AquilinaGreta@users.noreply.github.com>
2022-01-24 16:10:23 +0100

    Update projetc.js

--- projetc.js
+++ projetc.js
@@ -6,44 +6,43 @@ d3.csv("Deaths_EU.csv").then(function(data){
 
     });
 
+    //Width and height
+    var width = 500;
+    var height = 400;
+    var transform = "translate(200,100)";
 
-    //CODE HERE
-    console.log(data[0]);
+    let svg = d3.select("body").append("svg")
+                    .attr("width", width)
+                    .attr("height", height)
+                    .attr("transform", transform)
+                    
+
+    //Define map projection
+    let geoJsonUrl = ''
+
+    var europeProjection = d3.geoMercator()
+                                .center([ 13, 52 ])
+                                .scale([ width / 1.5 ])
+                                .translate([ width / 2, height / 2 ])
+
+    //Define path generator
+    var pathGenerator = d3.geoPath().projection(europeProjection)
+    geoJsonUrl = "https://gist.githubusercontent.com/spiker830/3eab0cb407031bf9f2286f98b9d0558a/raw/7edae936285e77be675366550e20f9166bed0ed5/europe_features.json"
+
+    d3.json(geoJsonUrl).then(geojson => {
+        // Tell D3 to render a path for each GeoJSON feature
+        svg.selectAll("path")
+            .data(geojson.features)
+            .enter()
+            .append("path")
+            .attr("d", pathGenerator) // This is where the magic happens
+            .attr("stroke", "grey") // Color of the lines themselves
+            .attr("fill", "white") // Color uses to fill in the lines
+        })
+
+  
+
 });
 
 
 
-//Width and height
-var width = 500;
-var height = 400;
-var transform = "translate(200,100)";
-
-let svg = d3.select("body").append("svg")
-                .attr("width", width)
-                .attr("height", height)
-                .attr("transform", transform)
-                
-
-//Define map projection
-let geoJsonUrl = ''
-
-var europeProjection = d3.geoMercator()
-                            .center([ 13, 52 ])
-                            .scale([ width / 1.5 ])
-                            .translate([ width / 2, height / 2 ])
-
-
-//Define path generator
-var pathGenerator = d3.geoPath().projection(europeProjection)
-geoJsonUrl = "https://gist.githubusercontent.com/spiker830/3eab0cb407031bf9f2286f98b9d0558a/raw/7edae936285e77be675366550e20f9166bed0ed5/europe_features.json"
-
-d3.json(geoJsonUrl).then(geojson => {
-    // Tell D3 to render a path for each GeoJSON feature
-    svg.selectAll("path")
-        .data(geojson.features)
-        .enter()
-        .append("path")
-        .attr("d", pathGenerator) // This is where the magic happens
-        .attr("stroke", "grey") // Color of the lines themselves
-        .attr("fill", "white") // Color uses to fill in the lines
-    })
