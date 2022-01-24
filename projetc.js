9f3c064e66a37120d371d4ed1633f05d72af6f0e
Simone Giordano <giordano.simoneit@gmail.com>
2022-01-24 16:21:37 +0100

    Update projetc.js

--- projetc.js
+++ projetc.js
@@ -8,42 +8,65 @@ d3.csv("Deaths_EU.csv").then(function(data){
 
 
     //CODE HERE
-    console.log(data[0]);
+    /*for(var i=0; i<data.length;i++){
+        if (data[i].Country == "Italy"){
+            console.log(data[i]);
+        }
+    }*/
+
+
+    var color = d3.scaleLinear()
+                .domain([0, 220])
+                .range(["#ffeda0","#f03b20"]);
+
+    //Width and height
+    var width = 500;
+    var height = 400;
+    var transform = "translate(200,100)";
+
+    let svg = d3.select("body").append("svg")
+                    .attr("width", width)
+                    .attr("height", height)
+                    .attr("transform", transform)
+                    
+
+    //Define map projection
+    let geoJsonUrl = ''
+
+    var europeProjection = d3.geoMercator()
+                                .center([ 13, 52 ])
+                                .scale([ width / 1.5 ])
+                                .translate([ width / 2, height / 2 ])
+
+
+    //Define path generator
+    var pathGenerator = d3.geoPath().projection(europeProjection)
+    
+    geoJsonUrl = "https://gist.githubusercontent.com/spiker830/3eab0cb407031bf9f2286f98b9d0558a/raw/7edae936285e77be675366550e20f9166bed0ed5/europe_features.json"
+    
+
+
+    d3.json(geoJsonUrl).then(geoJson=> {
+        // Tell D3 to render a path for each GeoJSON feature
+        svg.selectAll("path")
+            .data(geoJson.features)
+            //console.log(geojson.feature)
+            .enter()
+            .append("path")
+            .attr("d", pathGenerator) // This is where the magic happens
+            .attr("stroke", "grey") // Color of the lines themselves
+            .attr("fill", function(d){
+                for(var i=0; i<data.length;i++){
+                    if (data[i].Country == d.properties.name ){
+                        return color(data[i].Unsafe_water_source);
+                    }
+                }
+                
+            });
+        });
+
 });
 
 
 
-//Width and height
-var width = 500;
-var height = 400;
-var transform = "translate(200,100)";
 
-let svg = d3.select("body").append("svg")
-                .attr("width", width)
-                .attr("height", height)
-                .attr("transform", transform)
-                
-
-//Define map projection
-let geoJsonUrl = ''
-
-var europeProjection = d3.geoMercator()
-                            .center([ 13, 52 ])
-                            .scale([ width / 1.5 ])
-                            .translate([ width / 2, height / 2 ])
-
-
-//Define path generator
-var pathGenerator = d3.geoPath().projection(europeProjection)
-geoJsonUrl = "https://gist.githubusercontent.com/spiker830/3eab0cb407031bf9f2286f98b9d0558a/raw/7edae936285e77be675366550e20f9166bed0ed5/europe_features.json"
-
-d3.json(geoJsonUrl).then(geojson => {
-    // Tell D3 to render a path for each GeoJSON feature
-    svg.selectAll("path")
-        .data(geojson.features)
-        .enter()
-        .append("path")
-        .attr("d", pathGenerator) // This is where the magic happens
-        .attr("stroke", "grey") // Color of the lines themselves
-        .attr("fill", "white") // Color uses to fill in the lines
-    })
