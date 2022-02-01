f56d116cfd5d299ab550a285cd186c518d3c0389
Simone Giordano <giordano.simoneit@gmail.com>
2022-02-01 17:37:38 +0100

    Update parallel.js

--- SCRIPT/parallel.js
+++ SCRIPT/parallel.js
@@ -26,17 +26,23 @@ d3.csv("DATASET/Deaths_EU.csv").then(function(data){
 
     let Array_Deaths = ["Unsafe_water_source","Unsafe_sanitation","Household_air_pollution_from_solid_fuels","Child_wasting","Low_birth_weight_for_gestation",         "Secondhand_smoke","Alcohol_use","Drug_use","Diet_low_in_fruits","Unsafe_sex","High_fasting_plasma_glucose","High_body_mass_index","High_systolic_blood_pressure",         "Smoking","Iron_deficiency","Vitamin_A_deficiency","Low_bone_mineral_density","Air_pollution","Outdoor_air_pollution","Diet_high_in_sodium"];
 
-    let Countries =["Albania","Austria","Belarus"]
+    //let Countries =["Albania","Austria","Belarus","Hungary"]
+    let Countries =["Albania","Austria","Belarus","Belgium","Bosnia and Herzegovina","Bulgaria","Croatia","Cyprus","Czech Republic","Denmark","Estonia","Finland","France",
+                    "Germany","Greece","Iceland","Ireland","Italy","Latvia","Lithuania","Luxembourg","Malta","Moldova","Montenegro","Netherlands",
+                    "Norway","Poland","Portugal","Romania","Serbia","Slovakia","Slovenia","Spain","Sweden","Switzerland","Ukraine","United Kingdom","Macedonia","Hungary"]
+    
 
-    let array_years = [1990, 1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017];
+
+    let years = [1990, 1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016,2017];
     
     //Range di colori preso al sito: https://hihayk.github.io/scale/#20/20/17/82/285/47/0/53/3C8C08/223/57/177/white
-    /*let Colors = ["#009179","#008291","#005B91","#003491","#000E91","#000091","#1E0091","#450091","#6B0091","#8F0090","#90006D","#900049","#900026","#8F0004","#8F1800",
+    let Colors = ["#009179","#008291","#005B91","#003491","#000E91","#000091","#1E0091","#450091","#6B0091","#8F0090","#90006D","#900049","#900026","#8F0004","#8F1800",
                   "#8E3B00","#8E5E01","#8E7F03", "#7B8D04", "#5B8D06","#3C8C08","#499112","#56951C","#629A26","#6D9F31","#79A43B","#83A845","#8EAD4F","#98B259","#A1B663",
-                    "#ABBB6D","#B3C077","#BCC582","#C3C98C","#CBCE96","#D2D3A0","#D7D7AA","#DCDAB4","#E1DDBE0","#E6E2C8","#EAE6D3"]*/
-    let Colors = ["#009179","#7B8D04","#DCDAB4"]
+                    "#ABBB6D","#B3C077","#BCC582","#C3C98C","#CBCE96","#D2D3A0","#D7D7AA","#DCDAB4","#E1DDBE0","#E6E2C8","#EAE6D3"]
+                    
+    //let Colors = ["#009179","#7B8D04","#DCDAB4"]
 
-    var years = [2000, 2001, 2002];
+    //var years = [2000, 2001, 2002];
 
 
     var mylist = document.getElementById("List_Deaths");
@@ -54,7 +60,7 @@ d3.csv("DATASET/Deaths_EU.csv").then(function(data){
 
          // set the dimensions and margins of the graph
         var margin = {top: 30, right: 50, bottom: 10, left: 50},
-        width = 460 - margin.left - margin.right,
+        width = 660 - margin.left - margin.right,
         height = 400 - margin.top - margin.bottom;
         
         var svg = d3.select("#parallel")
@@ -74,20 +80,24 @@ d3.csv("DATASET/Deaths_EU.csv").then(function(data){
         }
         var addendo =0;
         for (var i=0; i<data.length;i++){
+
             if(Countries.includes(data[i].Country)){
                 if(years.includes(data[i].Year)){
 
                     for(l=0; l<years.length ; l++){
                         var year_ = years[l];
+                        
                         addendo = data[i+l][death_Selected];
                         
                         Dict_Of_TotalDeath_Year[year_] = Dict_Of_TotalDeath_Year[year_] + addendo;
 
                         }
-                        i = i+years.length;
+                        
+                        console.log(data[i].Country)
+                        console.log(data[i].Year)
+                        i = i+years.length-1;
 
                     }
-
                     
                 }
 
@@ -121,12 +131,12 @@ d3.csv("DATASET/Deaths_EU.csv").then(function(data){
 
             // first every group turns grey
             d3.selectAll(".line")
-                .transition().duration(200)
+                .transition().duration(100)
 
-                .style("opacity", "0.2")
+                .style("opacity", "0.1")
             // Second the hovered country takes its color
             d3.selectAll("." + selectedCountry)
-                .transition().duration(200)
+                .transition().duration(100)
 
                 .style("opacity", "1")
         }
@@ -157,7 +167,7 @@ d3.csv("DATASET/Deaths_EU.csv").then(function(data){
                             }
                             dict[data[i].Country]=Points_Of_Countries
                             Points_Of_Countries = []
-                            i = i+years.length;
+                            i = i+years.length-1;
 
                         }
                         
