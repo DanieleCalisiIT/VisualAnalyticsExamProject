8a02cb2087d43284ed335308727e50dc177bd597
Simone Giordano <giordano.simoneit@gmail.com>
2022-02-08 15:08:21 +0100

    Update parallel.js

--- SCRIPT/parallel.js
+++ SCRIPT/parallel.js
@@ -35,7 +35,7 @@ d3.csv("DATASET/Deaths_EU.csv").then(function(data){
 
     //let years = [1990, 1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016,2017];
     
-    let yearsPrevision = [2012,2013,2014,2015,2016,2017]
+    let yearsPrevision = [2015,2016,2017]
     //var numYearsToPredict = 2;
 
     var numYearsToPredict =  0;
