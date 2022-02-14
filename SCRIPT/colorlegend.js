f800319e900b63f10923661d663611a99258afe4
Simone Giordano <giordano.simoneit@gmail.com>
2022-02-14 10:19:08 +0100

    Update colorlegend.js

--- SCRIPT/colorlegend.js
+++ SCRIPT/colorlegend.js
@@ -29,7 +29,7 @@
         "Germany","Greece","Hungary","Iceland","Ireland","Italy","Latvia","Lithuania","Luxembourg","Macedonia","Malta","Moldova","Montenegro","Netherlands",
         "Norway","Poland","Portugal","Romania","Serbia","Slovakia","Slovenia","Spain","Sweden","Switzerland","Ukraine","United Kingdom"];  //39  
 
-    var SelectedCountries =[];  
+    
 
     var DistinctColors = ["#a6cee3","#1f78b4","#b2df8a","#33a02c","#fb9a99","#e31a1c","#fdbf6f","#ff7f00","#cab2d6","#6a3d9a"] //10
 
@@ -47,80 +47,81 @@
     slider.addEventListener('change', Change_In_The_ColorLegend);*/
 
 
-    function Change_In_The_ColorLegend(){
+function Change_In_The_ColorLegend(){
+    var SelectedCountries =[];  
 
-        d3.selectAll("#colorlegendSVG").remove();
-                
-        var svg = d3.select("#colorlegend")
-                    .append("svg")
-                    .attr("id", "colorlegendSVG")
-                    .attr("width", width + margin.left + margin.right)
-                    .attr("height", height + margin.top + margin.bottom)
-                    /*.append("g")
-        
-                    .attr("transform","translate(" + margin.left + "," + margin.top + ")");*/    //forse da rimettere
-        
-        var numero_Country_Brushed =  document.getElementById("number_of_Country_Selected").value
-
-            if(numero_Country_Brushed>0){
-                for(let l=1; l<=numero_Country_Brushed;l++){
-                    var base = "Selected_Country_"
-                    var id_country = base.concat(l)
-                    country_parsed = document.getElementById(id_country).getAttribute("value")
-                    SelectedCountries.push(country_parsed)
-                }
+    d3.selectAll("#colorlegendSVG").remove();
+           
+    var svg = d3.select("#colorlegend")
+                .append("svg")
+                .attr("id", "colorlegendSVG")
+                .attr("width", width + margin.left + margin.right)
+                .attr("height", height + margin.top + margin.bottom)
+                /*.append("g")
     
+                .attr("transform","translate(" + margin.left + "," + margin.top + ")");*/    //forse da rimettere
+    
+    var numero_Country_Brushed =  document.getElementById("number_of_Country_Selected").value
+
+        if(numero_Country_Brushed>0){
+            for(let l=1; l<=numero_Country_Brushed;l++){
+                var base = "Selected_Country_"
+                var id_country = base.concat(l)
+                country_parsed = document.getElementById(id_country).getAttribute("value")
+                SelectedCountries.push(country_parsed)
             }
+
+        }
+    
+    
+    
+    
+    //fill empty spaces with dummies
+    let gap = SelectedCountries.length
+    for(var i = 0; i < (10-gap); i ++){
+        SelectedCountries.push("Dummy" + i);
+    }
+    console.log(SelectedCountries)
+    var color = d3.scaleOrdinal()
+                .domain(SelectedCountries)  
+                .range(DistinctColors)  //aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+
+    svg.append("text").attr("x", "19%").attr("y", "5%").text("DYNAMIC").style("font-size", "20px").style("font-family", "American Typewriter, serif")   
+    svg.append("text").attr("x", "13%").attr("y", "9%").text("COUNTRIES").style("font-size", "20px").style("font-family", "American Typewriter, serif")   
+    svg.append("text").attr("x", "22%").attr("y", "13%").text("LEGEND").style("font-size", "20px").style("font-family", "American Typewriter, serif")   
+    for(var i = 0; i < 10; i++){
+
+        let country = SelectedCountries[i]
         
-        console.log(SelectedCountries)
-        
-        //fill empty spaces with dummies
-        let gap = SelectedCountries.length
-        for(var i = 0; i < (10-gap); i ++){
-            SelectedCountries.push("Dummy" + i);
+        let countryName = country
+        if ((country.charAt(0) == "D") && (country.charAt(1) == "u")){  //check that is dummy
+            countryName = "NULL";
         }
 
-        var color = d3.scaleOrdinal()
-                    .domain(SelectedCountries)  
-                    .range(DistinctColors)  //aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-
-        svg.append("text").attr("x", "19%").attr("y", "5%").text("DYNAMIC").style("font-size", "20px").style("font-family", "American Typewriter, serif")   
-        svg.append("text").attr("x", "13%").attr("y", "9%").text("COUNTRIES").style("font-size", "20px").style("font-family", "American Typewriter, serif")   
-        svg.append("text").attr("x", "22%").attr("y", "13%").text("LEGEND").style("font-size", "20px").style("font-family", "American Typewriter, serif")   
-
-        for(var i = 0; i < 10; i++){
-
-            let country = SelectedCountries[i]
-            
-            let countryName = country
-            if ((country.charAt(0) == "D") && (country.charAt(1) == "u")){  //check that is dummy
-                countryName = "NULL";
-            }
-
-            if (country == "Bosnia and Herzegovina"){
-                countryName = "Bosnia and H."
-            }
-                
-
-            let squaresSeparation = 8 * i
-            
-            
-            svg.append("rect").attr("x", "19%").attr("y", (19 + squaresSeparation) + "%").attr("width", "10%").attr("height", "2.8%").style("fill", color(country))
-
-            if(countryName == "NULL")
-                svg.append("text").attr("x", "33%").attr("y", (20.7 + squaresSeparation) + "%").text(countryName).style("font-size", "16px").attr("alignment-baseline","middle").style("opacity", .5);
-            else
-                svg.append("text").attr("x", "33%").attr("y", (20.7 + squaresSeparation) + "%").text(countryName).style("font-size", "16px").attr("alignment-baseline","middle")
-
-            //svg.append("rect").attr("x", 130 * i).attr("y",startingHighRect).attr("width", 15).attr("height", 15).style("fill", color(country))
-            //svg.append("text").attr("x", 20 + (130 * i)).attr("y", startingHighRect + 10).text(country).style("font-size", "14px").attr("alignment-baseline","middle")
-        
-
-        
+        if (country == "Bosnia and Herzegovina"){
+            countryName = "Bosnia and H."
         }
+            
+
+        let squaresSeparation = 8 * i
+        
+        
+        svg.append("rect").attr("x", "19%").attr("y", (19 + squaresSeparation) + "%").attr("width", "10%").attr("height", "2.8%").style("fill", color(country))
+
+        if(countryName == "NULL")
+            svg.append("text").attr("x", "33%").attr("y", (20.7 + squaresSeparation) + "%").text(countryName).style("font-size", "16px").attr("alignment-baseline","middle").style("opacity", .5);
+        else
+            svg.append("text").attr("x", "33%").attr("y", (20.7 + squaresSeparation) + "%").text(countryName).style("font-size", "16px").attr("alignment-baseline","middle")
+
+        //svg.append("rect").attr("x", 130 * i).attr("y",startingHighRect).attr("width", 15).attr("height", 15).style("fill", color(country))
+        //svg.append("text").attr("x", 20 + (130 * i)).attr("y", startingHighRect + 10).text(country).style("font-size", "14px").attr("alignment-baseline","middle")
+    
 
     
     }
-    Change_In_The_ColorLegend();
+    //console.log(SelectedCountries)
+
+}
+Change_In_The_ColorLegend();
 /*
 });*/
